5260c65713a1ecda01452bf566aeb7bdc2c75148
Meg7373 <meaghannlw@gmail.com>
2025-12-06 11:55:39 -0500

    update javascript

--- main.js
+++ main.js
@@ -6,5 +6,18 @@ document.getElementById('search-form').addEventListener('submit', async (e) => {
 
     try {
         const response = await fetch(apiUrl);
+        const data = await response.json();
+        const gifResultsDiv = document.getElementById('gif-results');
+        gifResultsDiv.innerHTML = '';
+
+        data.data.forEach(gif => {
+            const img = document.createElement('img');
+            img.scr = gif.images.fixed_height.url;
+            img.alt = gif.title;
+            gifResultsDiv.appendChild(img);
+        });
+    } catch (error) {
+        console.error('Error fetching GIFS:', error);
+        gifResultsDiv.innerHTML = '<p>Error loading GIFS. Please try again.</p>';
     }
-})
+});
