4292cd037833c210a8f586ada0b3867ccf9b1824
Meg7373 <meaghannlw@gmail.com>
2025-12-08 16:37:00 -0500

    update

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 document.getElementById('search-form').addEventListener('submit', async (e) => {
     e.preventDefault();
     const searchTerm = document.getElementById('search-input').value;
-    const apiKey = 'FSyEmnPbhNQmyLUGJRWirAgWeiFaxsQm'; // Replace with your actual API key
+    const apiKey = 'FSyEmnPbhNQmyLUGJRWirAgWeiFaxsQm'; 
     const apiUrl = `https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${apiKey}&limit=25`;
 
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
         const gifResultsDiv = document.getElementById('gif-results');
-        gifResultsDiv.innerHTML = ''; // Clear previous results
+        gifResultsDiv.innerHTML = '';
 
         data.data.forEach(gif => {
             const img = document.createElement('img');
@@ -21,3 +21,7 @@ document.getElementById('search-form').addEventListener('submit', async (e) => {
         gifResultsDiv.innerHTML = '<p>Error loading GIFs. Please try again.</p>';
     }
 });
+
+function resetForm () {
+    document.getElementById("gif-results").reset();
+}
